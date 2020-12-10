23cd5b99685b4a33d358a22d311a77331dee3ca5
Johan Sjöland <johan@sjoland.com>
2020-12-10 11:58:58 +0100

    Added directives

---
+++ src/BladeServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ABetter\Core;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class BladeServiceProvider extends ServiceProvider {
+
+    public function boot() {
+
+		// Shortcuts
+		Blade::directive('notempty', function($expression){
+			return "<?php if(!empty($expression)): ?>";
+		});
+		Blade::directive('ifnotempty', function($expression){
+			return "<?php if(!empty($expression)): ?>";
+		});
+		Blade::directive('ifx', function($expression){
+			return "<?php if(!empty($expression)): ?>";
+		});
+		Blade::directive('endnotempty', function($expression){
+			return '<?php endif; ?>';
+		});
+		Blade::directive('endifnotempty', function($expression){
+			return '<?php endif; ?>';
+		});
+		Blade::directive('endifx', function($expression){
+			return '<?php endif; ?>';
+		});
+
+    }
+
+    public function register() {
+        //
+    }
+
+}
